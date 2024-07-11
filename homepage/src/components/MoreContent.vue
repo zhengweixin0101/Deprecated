dd90abb1494d5a7355aabd491de2f98a3589b146
Weexy <zhengweixin0101@outlook.com>
2024-07-11 13:06:05 +0800

    1

---
+++ homepage/src/components/MoreContent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="more-content">这里还能放些什么东西？给点建议呗~</div>
+</template>
+
+<style lang="scss" scoped>
+.more-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ homepage/src/views/MoreSet/index.vue
@@ -0,0 +1,227 @@
+<template>
+  <div class="set" @mouseenter="closeShow = true" @mouseleave="closeShow = false" @click.stop>
+    <transition name="el-fade-in-linear">
+      <close-one
+        class="close"
+        theme="filled"
+        size="28"
+        fill="#ffffff60"
+        v-show="closeShow"
+        @click="store.setOpenState = false"
+      />
+    </transition>
+    <el-row :gutter="40">
+      <el-col :span="12" class="left">
+        <div class="logo text-hidden">
+          <span class="bg">{{ siteUrl[0] }}</span>
+          <span class="sm">.{{ siteUrl[1] }}</span>
+        </div>
+        <el-card class="update">
+          <template #header>
+            <div class="card-header">
+              <span>修改日志</span>
+            </div>
+          </template>
+          <div class="upnote">
+            <div class="about">
+              <p>本站由 
+                <a href="https://weexy.cn/">Weexy</a> 
+                基于 
+                <a href="https://github.com/imsyy/home">imsyy</a> 
+                大佬的个人主页进行二改</p>
+              <br/>
+            </div>
+            <div v-for="item in upData.new" :key="item" class="uptext">
+              <add-one theme="outline" size="22" />
+              {{ item }}
+            </div>
+            <div v-for="item in upData.fix" :key="item" class="uptext">
+              <bug theme="outline" size="22" />
+              {{ item }}
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="12" class="right">
+        <div class="title">
+          <setting-two theme="filled" size="28" fill="#ffffff60" />
+          <span class="name">全局设置</span>
+        </div>
+        <Set />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script setup>
+import { CloseOne, SettingTwo, GithubOne, AddOne, Bug } from "@icon-park/vue-next";
+import { mainStore } from "@/store";
+import Set from "@/components/Set.vue";
+import config from "@/../package.json";
+
+const store = mainStore();
+const closeShow = ref(false);
+
+// 站点链接
+const siteUrl = computed(() => {
+  const url = import.meta.env.VITE_SITE_URL;
+  if (!url) return "imsyy.top".split(".");
+  // 判断协议前缀
+  if (url.startsWith("http://") || url.startsWith("https://")) {
+    const urlFormat = url.replace(/^(https?:\/\/)/, "");
+    return urlFormat.split(".");
+  }
+  return url.split(".");
+});
+
+// 更新日志
+const upData = reactive({
+  new: [
+    "新增我自己的随机图片API作为壁纸",
+  ],
+  fix: ["删除两个失效壁纸选项", "删除部分内容，更简洁", "根据个人审美修改部分细节"],
+});
+
+// 跳转源代码仓库
+const jumpTo = (url) => {
+  window.open(url);
+};
+</script>
+
+<style lang="scss" scoped>
+.set {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  width: 80%;
+  height: 80%;
+  background: rgb(255 255 255 / 40%);
+  border-radius: 6px;
+  padding: 40px;
+
+  .close {
+    position: absolute;
+    top: 14px;
+    right: 14px;
+    width: 28px;
+    height: 28px;
+
+    &:hover {
+      transform: scale(1.2);
+    }
+
+    &:active {
+      transform: scale(1);
+    }
+  }
+
+  .el-row {
+    height: 100%;
+    flex-wrap: nowrap;
+
+    .left {
+      height: 100%;
+      padding-left: 40px !important;
+      padding-bottom: 20px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      .logo {
+        transform: translateY(-8%);
+        font-family: "Pacifico-Regular";
+        padding-left: 22px;
+        width: 100%;
+        height: 260px;
+        min-height: 140px;
+        .bg {
+          font-size: 5rem;
+        }
+
+        .sm {
+          margin-left: 6px;
+          font-size: 2rem;
+        }
+      }
+
+      .version {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        .num {
+          font-size: 2rem;
+          font-family: "Pacifico-Regular";
+        }
+
+        .github {
+          width: 24px;
+          height: 24px;
+          margin-left: 12px;
+          margin-top: 6px;
+
+          &:hover {
+            transform: scale(1.2);
+          }
+        }
+      }
+
+      .update {
+        margin-top: 30px;
+        height: 100%;
+
+        :deep(.el-card__body) {
+          height: 100%;
+
+          .upnote {
+            padding: 20px;
+            height: calc(100% - 56px);
+            overflow-y: auto;
+
+            .uptext {
+              display: flex;
+              flex-direction: row;
+              align-items: center;
+              padding-bottom: 16px;
+
+              &:nth-last-of-type(1) {
+                padding: 0;
+              }
+
+              .i-icon {
+                width: 22px;
+                height: 22px;
+                margin-right: 8px;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    .right {
+      height: 100%;
+      padding-right: 40px !important;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+
+      .title {
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+        font-size: 18px;
+        margin-bottom: 16px;
+
+        .i-icon {
+          width: 28px;
+          height: 28px;
+          margin-right: 6px;
+        }
+      }
+    }
+  }
+}
+</style>
