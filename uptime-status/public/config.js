158dc1ab5a7443c7d87a2e92ec9137a8fb816c67
Weexy <zhengweixin0101@outlook.com>
2024-07-11 13:06:17 +0800

    1

---
+++ uptime-status/public/config.js
@@ -0,0 +1,32 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '服务状态 | Weexy',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm796685742-8b1d023fcc295ba6e9777eb3',
+    'm796686319-704823edefc45bc74a1af81f',
+    'm796686339-b0b400d94f8602be3aac2969',
+    'm796686334-c999286183333f1843134bf0',
+    'm796686329-12dc552a6c2cbd719cd570de',
+    'm796688043-437218713e6d845a076a87b7',
+    'm796688046-1b02bd761fbf1439375855f1',
+    'm796688086-9ffa0716f0dcc1d69c39f82e',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+//    {
+//      text: 'Home',
+//      url: 'https://weexy.cn/'
+//    },
+  ],
+};
